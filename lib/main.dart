b604ce64d005bf50200565164b1a6505a5214402
Akshay Gupta <kitallis@gmail.com>
2023-02-27 18:00:00 +0530

    Add icons for JR shinkansen pass-thrus

--- lib/main.dart
+++ lib/main.dart
@@ -37,38 +37,59 @@ CupertinoDynamicColor textColor() {
 
 const title = "~ ueno (上野) aesthetic ~ ";
 
-// japanese, english, asset-name, distance from prev station, total distance
+// japanese, english, asset-name, distance from prev station, total distance, shinkansen pass-thru
 const stations = [
-  ["品川", "Shinagawa", "shinagawa", "0.9", "0.0"],
-  ["大崎", "Ōsaki", "osaki", "2.0", "2.0"],
-  ["五反田", "Gotanda", "gotanda", "0.9", "2.9"],
-  ["目黒", "Meguro", "meguro", "1.2", "4.1"],
-  ["恵比寿", "Ebisu", "ebisu", "1.5", "5.6"],
-  ["渋谷", "Shibuya", "shibuya", "1.6", "7.2"],
-  ["原宿", "Harajuku", "harajuku", "1.2", "8.4"],
-  ["代々木", "Yoyogi", "yoyogi", "1.5", "9.9"],
-  ["新宿", "Shinjuku", "shinjuku", "0.7", "10.6"],
-  ["新大久保", "Shin-Ōkubo", "shinokubo", "1.3", "11.9"],
-  ["高田馬場", "Takadanobaba", "takadanobaba", "1.4", "13.3"],
-  ["目白", "Mejiro", "mejiro", "0.9", "14.2"],
-  ["池袋", "Ikebukuro", "ikebukuro", "1.2", "15.4"],
-  ["大塚", "Ōtsuka", "otsuka", "1.8", "17.2"],
-  ["巣鴨", "Sugamo", "sugamo", "1.1", "18.3"],
-  ["駒込", "Komagome", "seseragi", "0.7", "19.0"],
-  ["田端", "Tabata", "tabata", "1.6", "20.6"],
-  ["西日暮里", "Nishi-Nippori", "nishinippori", "0.8", "21.4"],
-  ["日暮里", "Nippori", "nippori", "0.5", "21.9"],
-  ["鶯谷", "Uguisudani", "uguisudani", "1.1", "23.0"],
-  ["上野", "Ueno", "ueno", "1.1", "24.1"],
-  ["御徒町", "Okachimachi", "okachimachi", "0.6", "24.7"],
-  ["秋葉原", "Akihabara", "akihabara", "1.0", "25.7"],
-  ["神田", "Kanda", "kanda", "0.7", "26.4"],
-  ["東京", "Tōkyō", "tokyo", "1.3", "27.7"],
-  ["有楽町", "Yūrakuchō", "yurakucho", "0.8", "28.5"],
-  ["新橋", "Shimbashi", "shimbashi", "1.1", "29.6"],
-  ["浜松町", "Hamamatsuchō", "hamamatsucho", "1.2", "30.8"],
-  ["田町", "Tamachi", "tamachi", "1.5", "32.3"],
-  ["高輪ゲートウェイ", "Takanawa Gateway", "tokyo", "1.3", "33.6"],
+  [
+    "品川",
+    "Shinagawa",
+    "shinagawa",
+    "0.9",
+    "0.0",
+    ["jrc"]
+  ],
+  ["大崎", "Ōsaki", "osaki", "2.0", "2.0", []],
+  ["五反田", "Gotanda", "gotanda", "0.9", "2.9", []],
+  ["目黒", "Meguro", "meguro", "1.2", "4.1", []],
+  ["恵比寿", "Ebisu", "ebisu", "1.5", "5.6", []],
+  ["渋谷", "Shibuya", "shibuya", "1.6", "7.2", []],
+  ["原宿", "Harajuku", "harajuku", "1.2", "8.4", []],
+  ["代々木", "Yoyogi", "yoyogi", "1.5", "9.9", []],
+  ["新宿", "Shinjuku", "shinjuku", "0.7", "10.6", []],
+  ["新大久保", "Shin-Ōkubo", "shinokubo", "1.3", "11.9", []],
+  ["高田馬場", "Takadanobaba", "takadanobaba", "1.4", "13.3", []],
+  ["目白", "Mejiro", "mejiro", "0.9", "14.2", []],
+  ["池袋", "Ikebukuro", "ikebukuro", "1.2", "15.4", []],
+  ["大塚", "Ōtsuka", "otsuka", "1.8", "17.2", []],
+  ["巣鴨", "Sugamo", "sugamo", "1.1", "18.3", []],
+  ["駒込", "Komagome", "seseragi", "0.7", "19.0", []],
+  ["田端", "Tabata", "tabata", "1.6", "20.6", []],
+  ["西日暮里", "Nishi-Nippori", "nishinippori", "0.8", "21.4", []],
+  ["日暮里", "Nippori", "nippori", "0.5", "21.9", []],
+  ["鶯谷", "Uguisudani", "uguisudani", "1.1", "23.0", []],
+  [
+    "上野",
+    "Ueno",
+    "ueno",
+    "1.1",
+    "24.1",
+    ["jre", "jrh"]
+  ],
+  ["御徒町", "Okachimachi", "okachimachi", "0.6", "24.7", []],
+  ["秋葉原", "Akihabara", "akihabara", "1.0", "25.7", []],
+  ["神田", "Kanda", "kanda", "0.7", "26.4", []],
+  [
+    "東京",
+    "Tōkyō",
+    "tokyo",
+    "1.3",
+    "27.7",
+    ["jrc", "jre", "jrh"]
+  ],
+  ["有楽町", "Yūrakuchō", "yurakucho", "0.8", "28.5", []],
+  ["新橋", "Shimbashi", "shimbashi", "1.1", "29.6", []],
+  ["浜松町", "Hamamatsuchō", "hamamatsucho", "1.2", "30.8", []],
+  ["田町", "Tamachi", "tamachi", "1.5", "32.3", []],
+  ["高輪ゲートウェイ", "Takanawa Gateway", "tokyo", "1.3", "33.6", []],
 ];
 
 class Home extends State {
@@ -219,8 +240,11 @@ class _MyHomePageState extends State<MyHomePage> {
     var stationNameEn = station[1];
     var stationNameJp = station[0];
     var totalDistanceCovered = "total covered: ${station[4]}kms";
+    var shinkansens = station[5];
     var coverImage = "assets/cover/${station[2]}.jpg";
     var prevStation = [];
+
+    if (station[5].length != 0) {}
 
     if (index == 0) {
       prevStation = stations[stations.length - 1];
@@ -276,6 +300,17 @@ class _MyHomePageState extends State<MyHomePage> {
                       fontStyle: FontStyle.normal,
                       backgroundColor: textBgColor(),
                       color: textColor()))),
+          ...List.generate(
+            shinkansens.length,
+            (index) => DropShadow(
+                blurRadius: 0.2,
+                borderRadius: 0,
+                spread: 1,
+                offset: const Offset(0.0, 1.0),
+                child: Image.asset(
+                    "assets/misc/shinkansen/${shinkansens[index]}.png",
+                    width: 25.0)),
+          ),
           Visibility(
               maintainState: true,
               maintainAnimation: true,
